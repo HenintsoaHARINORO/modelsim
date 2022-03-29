885de61724b81e505be644d5571c4d3c0489fc07
Henintsoa Harinoro <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-03-29 16:59:04 +0300

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Before 
+```shell
+xauth list
+```
+```shell
+cp .env.example .env 
+```
+Change .env the number
+# To start
+```shell
+docker-compose up
+```
+# To stop
+```shell
+Cntr+C
+```
---
+++ entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+xauth add henintsoa-X555LF/unix$DISPLAY MIT-MAGIC-COOKIE-1 $SOME_NUMBER;
+xauth add henintsoa-X555LF/unix$DISPLAY MIT-MAGIC-COOKIE-1 $SOME_NUMBER;
+vsim
+tail -f /dev/null
